94d542558f7a62edac63dff8ff48d008ad9178df
NiiAmui <samniiamui@gmail.com>
2022-06-02 14:54:14 -0700

    final

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- *  * main - prints the phrase with printf()
- *   *
- *    * Return : Always 0 (Success)
- *     */
+ * main - Entry poin
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
